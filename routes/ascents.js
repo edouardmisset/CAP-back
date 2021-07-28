1536c3882faa947d459fe398857ecd3c82f39260
Edouard Misset <edouardmisset@gmail.com>
2021-07-28 18:06:35 +0200

    Changed model

--- routes/ascents.js
+++ routes/ascents.js
@@ -4,8 +4,7 @@ const AscentModel = require('../models/AscentModel')
 // Get all ascents
 ascentsRouter.get('/', async (req, res) => {
   try {
-    const allAscents = await AscentModel.findAll()
-    res.send(allAscents)
+    res.send(await AscentModel.findAll())
   } catch (err) {
     console.error(err)
     res.status(500).send(err)
@@ -13,25 +12,25 @@ ascentsRouter.get('/', async (req, res) => {
 })
 
 // Get ascents by grade by style
-ascentsRouter.get('/by-grade-by-style', async (req, res) => {
-  try {
-    const firstGo = await AscentModel.numberOfAscentsFirstGoByGrade()
-    const secondGoAndMore =
-      await AscentModel.numberOfAscentsSecondGoOrMoreByGrade()
-    const topoGrades = await AscentModel.getGrades()
-    res.send({
-      x: topoGrades,
-      y: [
-        { name: 'Second Go', data: secondGoAndMore, color: '#ffdc00' },
-        { name: 'First Go', data: firstGo, color: '#2ecc40' },
-      ],
-      title: 'Number of Ascents by Grade and Style',
-    })
-  } catch (err) {
-    console.error(err)
-    res.status(500).send(err)
-  }
-})
+// ascentsRouter.get('/by-grade-by-style', async (req, res) => {
+//   try {
+//     const firstGo = await AscentModel.numberOfAscentsFirstGoByGrade()
+//     const secondGoAndMore =
+//       await AscentModel.numberOfAscentsSecondGoOrMoreByGrade()
+//     const topoGrades = await AscentModel.getGrades()
+//     res.send({
+//       x: topoGrades,
+//       y: [
+//         { name: 'Second Go', data: secondGoAndMore, color: '#ffdc00' },
+//         { name: 'First Go', data: firstGo, color: '#2ecc40' },
+//       ],
+//       title: 'Number of Ascents by Grade and Style',
+//     })
+//   } catch (err) {
+//     console.error(err)
+//     res.status(500).send(err)
+//   }
+// })
 
 // Post a new ascent
 ascentsRouter.post('/', async (req, res) => {
